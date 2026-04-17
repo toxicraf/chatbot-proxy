a9aef852d33afe3eec652ddab1674e56e1181ed3
Zoran Dimitrijevic <toxicraf@gmail.com>
2026-04-17 17:13:49 +0200

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 app.use(express.json());
